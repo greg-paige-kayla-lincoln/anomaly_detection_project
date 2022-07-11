1b0f944854395ef38109bffd562c40a1ae631da3
Lincoln Muriithi <lincoln.muriithi11@gmail.com>
2022-07-11 12:59:09 -0500

    acquire file

--- acquire.py
+++ acquire.py
@@ -11,16 +11,7 @@ from env import host, user, password
 
 import warnings
 warnings.filterwarnings("ignore")
-from math import sqrt
-from scipy import stats
-from statsmodels.formula.api import ols
-from sklearn.metrics import mean_squared_error, explained_variance_score
-from sklearn.linear_model import LinearRegression
-from sklearn.feature_selection import RFE, SelectKBest, f_regression
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler, QuantileTransformer
 
 ###################### Acquire and Clean Data ######################
 #below are the credentials to the SQL db  linked to a .env file.
@@ -35,9 +26,7 @@ def get_connection(db, user=user, host=host, password=password):
 #the function 
 def get_cohort_sql():
 
-    ''' this function calls a sql file from the codeup database and creates a data frame from the zillow db. The data include these columns;
-        bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, fips and other parameters that are needed to acquire the data needed for the project.
-        
+    ''' this function calls a sql file from the codeup database and creates a data frame from the curriculum db.     
     '''
     query = '''SELECT * FROM logs   LEFT JOIN  cohorts ON logs.cohort_id = cohorts.id;'''
             
@@ -50,7 +39,7 @@ def get_cohort_sql():
  #this function gets the zillow data thats already saved as a csv file from the zillow data base        
 def get_cohort_df():
     '''
-    This function reads in zillow data from Codeup database, writes data to
+    This function reads in curriculum log data from Codeup database, writes data to
     a csv file if a local file does not exist, and returns a df.
     '''
     if os.path.isfile('curriculum.csv'):
