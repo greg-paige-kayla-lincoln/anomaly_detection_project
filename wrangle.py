ef0109facbb4a7911fc347bc07b733f1ae859962
Kayla Brock <kayla.marie.brock@gmail.com>
2022-07-11 14:09:21 -0500

    wrangle update

--- wrangle.py
+++ wrangle.py
@@ -1,3 +1,4 @@
+import pandas as pd
 def wrangle_df(df):  
     df.set_index('date', inplace=True)
     df.program_id = df.program_id.map({1.0:'full_stack_php', 2.0:'full_stack_java', 3.0:'data_science', 4.0:'front_end_programming'})
