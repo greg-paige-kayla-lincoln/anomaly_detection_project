2259de537e0c57b6334bd5aee3e6e5bad103cc43
Greg Maggard <gregamaggard@gmail.com>
2022-07-11 13:47:25 -0500

    Updating acquire.py.

--- acquire.py
+++ acquire.py
@@ -1,59 +1,23 @@
-################ Libraries and documents needed for this project ################
-import warnings
-warnings.filterwarnings("ignore")
-
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pandas as pd 
 import os
-from env import host, user, password
 
-import warnings
-warnings.filterwarnings("ignore")
-from sklearn.model_selection import train_test_split
+from env import get_db_url
 
-###################### Acquire and Clean Data ######################
-#below are the credentials to the SQL db  linked to a .env file.
-def get_connection(db, user=user, host=host, password=password):
-    '''
-    This function uses my info from my env file to
-    create a connection url to access the Codeup db.
-    It takes in a string name of a database as an argument.
-    '''
-    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
-# ----------------------------------------------------------------- #
-#the function 
-def get_cohort_sql():
+def get_db_log_data():
 
-    ''' this function calls a sql file from the codeup database and creates a data frame from the curriculum db.     
-    '''
-    query = '''SELECT * FROM logs LEFT JOIN cohorts ON logs.cohort_id = cohorts.id;'''
-            
+    '''If previously-generated CSV of logs exists, returns DataFrame of cached log data. If no CSV exists, 
+    accesses Codeup database to pull logs of curriculum access, returns a DataFrame of acquired logs, and caches data in CSV
+    format for future use.'''
+    filename = "db_log_data.csv"
 
-    df = pd.read_sql(query, get_connection('curriculum_logs'))
-    #creating a csv for easy access 
-    return df
-
-
- #this function gets the zillow data thats already saved as a csv file from the Codeup database        
-def get_cohort_df():
-    '''
-    This function reads in curriculum log data from Codeup database, writes data to
-    a csv file if a local file does not exist, and returns a df.
-    '''
-    if os.path.isfile('curriculum.csv'):
-        
-        # If csv file exists read in data from csv file.
-        df = pd.read_csv('curriculum.csv', index_col=0)
-        
+    if os.path.isfile(filename):
+        return pd.read_csv(filename, index_col = 0)
     else:
-        
-        # Read fresh data from db into a DataFrame
-        df = get_cohort_sql()
-        
-        # Cache data
-        df.to_csv('curriculum.csv')
-        
-    return df
+        # read the SQL query into a DataFrame
+        df = pd.read_sql('''SELECT * FROM logs LEFT JOIN cohorts ON logs.cohort_id = cohorts.id;''', get_db_url('curriculum_logs'))
 
+        # Write that DataFrame to CSV to cache for future use.
+        df.to_csv(filename)
+
+    # Return the DataFrame containing the log data:
+    return df
