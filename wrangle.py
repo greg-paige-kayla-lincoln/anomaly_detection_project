30d5c11567868ea37ae613a641c29c8fd7fdf089
Greg Maggard <gregamaggard@gmail.com>
2022-07-11 14:43:51 -0500

    Making some progress, updating wrangle again.

--- wrangle.py
+++ wrangle.py
@@ -2,12 +2,9 @@ import pandas as pd
 
 def wrangle_df(df):
     
-    '''This function takes in a DataFrame of curriculum access logs, does some basic wrangling like
-    index-setting, mapping program codes to program ID numbers, drops unneeded or duplicated columns, 
+    '''This function takes in a DataFrame of curriculum access logs, does some basic wrangling like 
+    mapping program codes to program ID numbers, drops unneeded or duplicated columns, 
     and changes date columns to the datetime data type.'''
-    
-    # Sets the index of the DataFrame to the date
-    df.set_index('date', inplace=True)
     
     #Updates program ID numbers to their respective program titles:
     df.program_id = df.program_id.map({1.0:'full_stack_php', 2.0:'full_stack_java', 3.0:'data_science', 4.0:'front_end_programming'})
@@ -16,7 +13,7 @@ def wrangle_df(df):
     df.drop(columns=['id','slack','deleted_at'],inplace = True)
     
     #Creates a list of columns containing relevant date data for logs:
-    date_cols = ['start_date', 'end_date', 'created_at', 'updated_at']
+    date_cols = ['date','time','start_date', 'end_date', 'created_at', 'updated_at']
     
     #Iterates through list of dates to change them to datetime data type:
     for col in date_cols: 
